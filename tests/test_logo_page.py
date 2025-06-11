18e5e19f31dc3c9e18e24d1ed335f23f547ccc5b
Forcecom92 <4621389@gmail.com>
2025-06-11 23:38:32 +0300

    First commit

---
+++ tests/test_logo_page.py
@@ -0,0 +1,15 @@
+import allure
+from pages.logo_page import LogoPage
+from data import URL
+
+
+class TestLogoPage:
+
+    @allure.title('Проверка возврата на домашнюю страницу по клику на логотип Самоката')
+    @allure.description('Кликаем на кнопку "Принять куки", далее на кнопку "Заказать", чтобы уйти с домашней страницы, а потом возвращаемся на нее обратно')
+    def test_logo_scooter(self, firefox):
+        logo_page = LogoPage(firefox)
+        logo_page.click_cookie()
+        logo_page.click_order_and_click_logo_scooter_back()
+
+        assert logo_page.get_current_url() == URL.FIRST_PAGE
